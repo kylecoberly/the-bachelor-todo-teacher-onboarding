6932c3f8ff0d943f1d9093c4537be21e800a8bfd
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-01 04:34:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,16 +3,16 @@ def get_first_name_of_season_winner(data, season)
   winner["name"].split(" ").first
 end
 
-def women
+def get_women data
   data.flat_map {|season, women| women}
 end
 
 def get_contestant_name(data, occupation)
-  women.find {|woman| woman["occupation"].eql? occupation}["name"]
+  get_women(data).find {|woman| woman["occupation"].eql? occupation}["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
 end
 
 def get_occupation(data, hometown)
