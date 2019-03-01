3a95cd09475e18b784686abe560a363a225d8172
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-01 04:31:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def women
-  data.flat_map(season, women)
+  data.flat_map {|season, women| women}
 end
 
 def get_contestant_name(data, occupation)
