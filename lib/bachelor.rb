989ee43ad3251e893e89be4a240c507dc06dd984
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-01 04:43:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,7 +14,6 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   get_women(data).reduce(0) {|hometown_count, woman|
-    p hometown_count, woman["hometown"], hometown
     hometown_count += 1 if woman["hometown"].eql? hometown
     hometown_count
   }
