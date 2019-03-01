b1d3d8badeffaf0c2a75f02754485b17f8793ed8
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-01 04:53:03 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,31 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find {|contestant| contestant["status"].eql? "Winner"}
+  winner["name"].split(" ").first
+end
+
+def get_women data
+  data.flat_map {|season, women| women}
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  get_women(data)
+    .find {|woman| woman["occupation"].eql? occupation}["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  get_women(data).reduce(0) {|hometown_count, woman|
+    hometown_count += 1 if woman["hometown"].eql? hometown
+    hometown_count
+  }
 end
 
 def get_occupation(data, hometown)
-  # code here
+  get_women(data)
+    .find {|woman| woman["hometown"].eql? hometown}["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  women = data[season]
+  total_ages = women.reduce(0) {|total_ages, woman| total_ages + woman["age"].to_f}
+  (total_ages / women.count).round
 end
