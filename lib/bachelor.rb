0a6f2e69f1f693380bdbacb9b3eba40035772bcd
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-01 04:51:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,5 +25,5 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data[season].reduce(0) {|total_ages, woman| total_ages + woman["age"].to_f}
 end
